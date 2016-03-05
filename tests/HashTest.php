ada359ffed1b536d06af55439c3c38a3ed8730ed
Steven Crothers <steven.crothers@gmail.com>
2016-03-05 16:57:30 -0500

    Added better test coverage to the hash testing.

--- tests/HashTest.php
+++ tests/HashTest.php
@@ -28,6 +28,86 @@ class HashTest extends PHPUnit_Framework_TestCase
 
     /**
      * @depends testGenericHash
+     * @expectedException SimpleAPISecurity\PHP\Exceptions\OutOfRangeException
+     * @expectedExceptionMessageRegExp #generateKey length range: \d+ to \d+#
+     */
+    public function testGenerateKeyExceptionHigh()
+    {
+        Hash::generateKey(Constants::GENERICHASH_KEYBYTES_MAX + 1);
+    }
+
+    /**
+     * @depends testGenericHash
+     * @expectedException SimpleAPISecurity\PHP\Exceptions\OutOfRangeException
+     * @expectedExceptionMessageRegExp #generateKey length range: \d+ to \d+#
+     */
+    public function testGenerateKeyExceptionLow()
+    {
+        Hash::generateKey(Constants::GENERICHASH_KEYBYTES_MIN - 1);
+    }
+
+    /**
+     * @depends testGenericHash
+     * @expectedException SimpleAPISecurity\PHP\Exceptions\InvalidTypeException
+     * @expectedExceptionMessage Expected integer parameter for generateKey
+     */
+    public function testGenerateKeyExceptionBadValue()
+    {
+        Hash::generateKey('test');
+    }
+
+    /**
+     * @depends testGenericHash
+     * @expectedException SimpleAPISecurity\PHP\Exceptions\OutOfRangeException
+     * @expectedExceptionMessageRegExp #hash length range: \d+ to \d+#
+     */
+    public function testGenerateHashExceptionHigh()
+    {
+        Hash::hash('test', '', Constants::GENERICHASH_BYTES_MAX + 1);
+    }
+
+    /**
+     * @depends testGenericHash
+     * @expectedException SimpleAPISecurity\PHP\Exceptions\OutOfRangeException
+     * @expectedExceptionMessageRegExp #hash length range: \d+ to \d+#
+     */
+    public function testGenerateHashExceptionLow()
+    {
+        Hash::hash('test', '', Constants::GENERICHASH_BYTES_MIN - 1);
+    }
+
+    /**
+     * @depends testGenericHash
+     * @expectedException SimpleAPISecurity\PHP\Exceptions\InvalidTypeException
+     * @expectedExceptionMessage Expected integer parameter for length in hash
+     */
+    public function testGenerateHashExceptionBadValue()
+    {
+        Hash::hash('test', '', 'test');
+    }
+
+    /**
+     * @depends testGenericHash
+     * @expectedException SimpleAPISecurity\PHP\Exceptions\InvalidTypeException
+     * @expectedExceptionMessage Expected string parameter for message in hash
+     */
+    public function testGenerateHashExceptionBadValueMessage()
+    {
+        Hash::hash(1);
+    }
+
+    /**
+     * @depends testGenericHash
+     * @expectedException SimpleAPISecurity\PHP\Exceptions\InvalidTypeException
+     * @expectedExceptionMessage Expected string parameter for key in hash
+     */
+    public function testGenerateHashExceptionBadValueKey()
+    {
+        Hash::hash('test', 1);
+    }
+
+    /**
+     * @depends testGenericHash
      */
     public function testKeyLength()
     {
@@ -57,6 +137,27 @@ class HashTest extends PHPUnit_Framework_TestCase
     }
 
     /**
+     * @depends testShortHash
+     * @expectedException SimpleAPISecurity\PHP\Exceptions\InvalidTypeException
+     * @expectedExceptionMessage Expected string parameter for message in shortHash
+     */
+    public function testShortHashBadMessage()
+    {
+        $key = Hash::generateKey(Constants::GENERICHASH_KEYBYTES_MIN);
+        Hash::shortHash(1, $key);
+    }
+
+    /**
+     * @depends testShortHash
+     * @expectedException SimpleAPISecurity\PHP\Exceptions\InvalidTypeException
+     * @expectedExceptionMessage Expected string parameter for key in shortHash
+     */
+    public function testShortHashBadKey()
+    {
+        Hash::shortHash('test', 1);
+    }
+
+    /**
      * @requires extension libsodium
      */
     public function passwordTest()
@@ -65,5 +166,28 @@ class HashTest extends PHPUnit_Framework_TestCase
         $passwordHash = Hash::hashPassword($password);
         $this->assertTrue(Hash::verifyPassword($password, $passwordHash));
         $this->assertFalse(Hash::verifyPassword('notcorrect', $passwordHash));
+    }
+
+    /**
+     * @depends passwordTest
+     * @expectedException SimpleAPISecurity\PHP\Exceptions\InvalidTypeException
+     * @expectedExceptionMessage Expected string parameter for password in verifyPassword
+     */
+    public function passwordTestBadPassword()
+    {
+        $password = 'testing';
+        $passwordHash = Hash::hashPassword($password);
+        Hash::verifyPassword(1, $passwordHash);
+    }
+
+    /**
+     * @depends passwordTest
+     * @expectedException SimpleAPISecurity\PHP\Exceptions\InvalidTypeException
+     * @expectedExceptionMessage Expected string parameter for passwordHash in verifyPassword
+     */
+    public function passwordTestBashPasswordHash()
+    {
+        $password = 'testing';
+        Hash::verifyPassword($password, 1);
     }
 }
